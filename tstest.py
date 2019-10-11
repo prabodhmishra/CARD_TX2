3a2b676c6c8a1f5ca66381d59a1944dd685cb121
Prabodh Mishra <pmishra@clemson.edu>
2019-10-11 14:38:20 -0400

    changes 2 to tstest.py

--- tstest.py
+++ tstest.py
@@ -6,13 +6,13 @@ import time
 
 while 1:
     # tegra stats
-    os.system('~/tegrastats -once | grep RAM > ./dash_files/log.txt')
+    os.system('~/tegrastats -once | grep RAM > ~/dash_files/log.txt')
     # GPU usage
-    os.system('cat /sys/devices/gpu.0/load >> ./dash_files/log.txt')
+    os.system('cat /sys/devices/gpu.0/load >> ~/dash_files/log.txt')
 
     # Number of decoded files
-    os.system('ls -1 ./soundfiles16/ | wc -l > ./dash_files/num_files.txt')
-    os.system('ls -1 ./logs/ | wc -l >> ./dash_files/num_files.txt') 
+    os.system('ls -1 ./soundfiles16/ | wc -l > ~/dash_files/num_files.txt')
+    os.system('ls -1 ./logs/ | wc -l >> ~/dash_files/num_files.txt') 
     
     time.sleep(1)
 
