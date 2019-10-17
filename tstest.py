abdc85e2724b95ec6eb27e2beda72931efbf0fe8
Prabodh Mishra <pmishra@clemson.edu>
2019-10-17 14:29:23 -0400

    added string conv

--- tstest.py
+++ tstest.py
@@ -3,21 +3,21 @@ import signal
 import os
 import time
 
-f = open("~/device_num.txt", "r")
+# f = open("~/device_num.txt", "r")
 
-device_num = f.read()
-
+# device_num = f.read()
+device_num = 3
 while 1:
     # tegra stats
-    os.system('~/tegrastats -once | grep RAM > ~/device' + device_num + '/log.txt')
+    os.system('~/tegrastats -once | grep RAM > ~/device' + str(device_num) + '/log.txt')
     # GPU usage
-    os.system('cat /sys/devices/gpu.0/load >> ~/device' + device_num + '/log.txt')
+    os.system('cat /sys/devices/gpu.0/load >> ~/device' + str(device_num) + '/log.txt')
 
     # Number of decoded files
-    os.system('ls -1 ./soundfiles/ | wc -l > ~/device' + device_num + '/num_files.txt')
-    os.system('ls -1 ./logs/ | wc -l >> ~/device' + device_num + '/num_files.txt') 
+    os.system('ls -1 ./soundfiles/ | wc -l > ~/device' + str(device_num) + '/num_files.txt')
+    os.system('ls -1 ./logs/ | wc -l >> ~/device' + str(device_num) + '/num_files.txt') 
     
     time.sleep(1)
 
-    os.system('scp -r ~/device' + device_num + ' mjwoo@172.22.44.52:/Users/mjwoo/PycharmProjects/CARD_dashboard')
+    os.system('scp -r ~/device' + str(device_num) + ' mjwoo@172.22.44.52:/Users/mjwoo/PycharmProjects/CARD_dashboard')
 
