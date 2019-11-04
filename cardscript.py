daf4a07296bc4ca5259a35c3722ce6825e4a2969
Prabodh Mishra <pmishra@clemson.edu>
2019-11-04 01:24:47 -0500

    updates for sending ASR Logs

--- cardscript.py
+++ cardscript.py
@@ -6,7 +6,7 @@ import datetime
 
 start = datetime.datetime.now()
 
-os.system("ffmpeg -f alsa -i hw:2 -f segment -segment_time 30 -loglevel quiet -ar 16000 soundfiles16/audio_%04d.wav &")
+os.system("ffmpeg -f alsa -i hw:2 -f segment -segment_time 05 -loglevel quiet -ar 16000 soundfiles16/audio_%04d.wav &")
 
 notifier = inotify.adapters.Inotify()
 notifier.add_watch('/home/nvidia/card/soundfiles16')
@@ -17,13 +17,13 @@ for event in notifier.event_gen():
     if event is not None:
         # print event      # uncomment to see all events generated
         if 'IN_CREATE' in event[1]:
-	     if event[3] != "audio_0000.wav":
-		inc = datetime.datetime.now()
+            if event[3] != "audio_0000.wav":
+		        inc = datetime.datetime.now()
              	print "file '{0}' created in '{1}'".format(event[3], event[2])
-		print "Elapsed time: " + str((inc - start).total_seconds())
-		os.system("free -m | awk 'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\\n\", $3,$2,$3*100/$2 }'")
-                os.system("top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\\n\", $(NF-2)}'")
+		        print "Elapsed time: " + str((inc - start).total_seconds())
+		        # os.system("free -m | awk 'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\\n\", $3,$2,$3*100/$2 }'")
+                # os.system("top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\\n\", $(NF-2)}'")
              	os.system("ffmpeg -i soundfiles16/" + tempaudio + " -loglevel quiet -ac 1 -ar 8000 soundfiles08/" + tempaudio)
-		templog = tempaudio[:-4] + ".log"
-		os.system("./decode.sh soundfiles08/" + tempaudio + " >> logs/" + templog)
-	     tempaudio = event[3]
+		        templog = tempaudio[:-4] + ".log"
+		        os.system("./decode.sh soundfiles08/" + tempaudio + " >> logs/" + templog)
+            tempaudio = event[3]
--- send_stats.py
+++ send_stats.py
@@ -6,6 +6,7 @@ import time
 f = open("/home/nvidia/device_num.txt", "r")
 
 device_num = str(f.readline(1))
+last_sent = 0
 
 while 1:
     # tegra stats
@@ -14,9 +15,18 @@ while 1:
     os.system('cat /sys/devices/gpu.0/load >> ~/device' + device_num + '/log.txt')
 
     # Number of decoded files
-    os.system('ls -1 ./soundfiles/ | wc -l > ~/device' + device_num + '/num_files.txt')
-    os.system('ls -1 ./logs/ | wc -l >> ~/device' + device_num + '/num_files.txt') 
-    
+    os.system('ls -1 ./soundfiles16/ | wc -l > ~/device' + device_num + '/num_files.txt')
+    os.system('ls -1 ./logs/ | wc -l >> ~/device' + device_num + '/num_files.txt')
+
+    audlogfiles = os.popen('ls -1 ./logs/ | wc -l').read()
+    cnt_audlogs = int(audlogfiles[:-1])
+    new_files = cnt_audlogs - last_sent
+    if new_files > 0:
+        os.system('rm ~/device' + device_num + '/asr/*')
+        for x in range(new_files):
+            os.system('cp ./logs/audio_' + '{:04d}'.format(last_sent+x) + '.log ~/device' + device_num + '/asr/')
+        last_sent += new_files
+
     time.sleep(1)
 
-    os.system('scp -r ~/device' + device_num + ' mjwoo@198.21.152.178:/Users/mjwoo/PycharmProjects/CARD_dashboard_github')
+    os.system('scp -r ~/device' + device_num + ' mjwoo@198.21.158.154:/Users/mjwoo/PycharmProjects/CARD_dashboard_github')
