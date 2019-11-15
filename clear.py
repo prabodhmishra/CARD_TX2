837bec126b6557e6400ddeba75c8bb1e9d2206ee
Prabodh Mishra <pmishra@clemson.edu>
2019-11-15 17:44:47 -0500

    changes to delete files in asr directory

--- clear.py
+++ clear.py
@@ -2,6 +2,9 @@ import os
 import datetime
 
 var = str(datetime.datetime.now())
+f = open("/home/nvidia/device_num.txt", "r")
+
+device_num = str(f.readline(1))
 
 '''
 os.system("mkdir oldfiles/run_16_1")
@@ -13,5 +16,5 @@ os.system("mv soundfiles08/* oldfiles/run_08_1")
 os.system("mv logs/* oldfiles/logs_1")
 '''
 os.system("rm soundfiles16/*")
-os.system("rm soundfiles08/*")
+os.system('rm ../device' + device_num + '/asr/*')
 os.system("rm logs/*")
